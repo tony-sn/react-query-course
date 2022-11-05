5a1187415b38e922fc0c325abc926e4794087606
tony-sn <57897153+tony-sn@users.noreply.github.com>
2022-11-06 09:28:41 +1100

    Merge: dev branch to main branch (v 1.0.0)
    * ci: change project to ts
    
    * ci: add eslint to project
    
    * chore: change prettier to eslint
    
    ci: change project to ts
    
    ci: add eslint to project
    
    refactor: typo ; at Label
    
    chore: change config files
    
    chore: add eslint config
    
    chore: remove yarn.lock

---
+++ src/components/IssueDetails.tsx
@@ -0,0 +1,82 @@
+import { useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
+import { relativeDate } from 'helpers/relativeDate'
+import { useUserData } from 'helpers/useUserData'
+import type { CommentProps } from 'interfaces/index'
+import { IssueHeader } from './IssueHeader'
+
+function useIssueData(issueNumber: string) {
+  return useQuery(['issues', issueNumber], () => {
+    return fetch(`/api/issues/${issueNumber}`).then(res => res.json())
+  })
+}
+
+function useIssueComments(issueNumber: string) {
+  return useQuery(['issues', issueNumber, 'comments'], () => {
+    return fetch(`/api/issues/${issueNumber}/comments`).then(res =>
+      res.json(),
+    )
+  })
+}
+
+function Comment({ comment, createdBy, createdDate }: Partial<CommentProps>) {
+  const userQuery = useUserData(createdBy as string)
+
+  if (userQuery.isLoading) {
+    return (
+      <div className="comment">
+        <div>
+          <div className="comment-header">Loading...</div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="comment">
+      <img src={userQuery.data.profilePictureUrl} alt="Commenter Avatar" />
+      <div>
+        <div className="comment-header">
+          <span>{userQuery.data.name}</span> commented{' '}
+          <span>{relativeDate(createdDate)}</span>
+        </div>
+        <div className="comment-body">{comment?.id}</div>
+      </div>
+    </div>
+  )
+}
+
+export default function IssueDetails() {
+  const { number } = useParams()
+  const issueQuery = useIssueData(number as string)
+  const commentsQuery = useIssueComments(number as string)
+
+  return (
+    <div className="issue-details">
+      {issueQuery.isLoading
+        ? (
+        <p>Loading issue...</p>
+          )
+        : (
+        <>
+          <IssueHeader {...issueQuery.data} />
+
+          <main>
+            <section>
+              {commentsQuery.isLoading
+                ? (
+                <p>Loading...</p>
+                  )
+                : (
+                    commentsQuery.data?.map((comment: { id: string }) => (
+                  <Comment key={comment.id} {...comment} />
+                    ))
+                  )}
+            </section>
+            <aside></aside>
+          </main>
+        </>
+          )}
+    </div>
+  )
+}
---
+++ src/components/LabelList.tsx
@@ -0,0 +1,39 @@
+import { useLabelsData } from 'helpers/useLabelsData'
+import type { LabelType, TODO_TYPEME } from 'interfaces/index'
+
+// FIXME: type for toggle function later
+export default function LabelList({
+  selected,
+  toggle,
+}: {
+  selected: LabelType[]
+  toggle: TODO_TYPEME
+}) {
+  const labelsQuery = useLabelsData()
+
+  return (
+    <div className="labels">
+      <h3>Labels</h3>
+      {labelsQuery.isLoading
+        ? (
+        <p>Loading...</p>
+          )
+        : (
+        <ul>
+          {labelsQuery.data.map((label: LabelType) => (
+            <li key={label.id}>
+              <button
+                onClick={() => toggle(label.id)}
+                className={`label ${
+                  selected.includes((label as TODO_TYPEME).id) ? 'selected ' : ''
+                }${label.color}`}
+              >
+                {label.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+          )}
+    </div>
+  )
+}
